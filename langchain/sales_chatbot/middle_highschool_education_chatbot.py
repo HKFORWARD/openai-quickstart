350aee55b36c8fccb9bf49b4de76c3643fccf547
Kun He <hekun830930@gmail.com>
2024-04-22 19:49:39 +0800

    first attempt using TabbedInterface

--- langchain/sales_chatbot/middle_highschool_education_chatbot.py
+++ langchain/sales_chatbot/middle_highschool_education_chatbot.py
@@ -7,7 +7,7 @@ from langchain.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
 
 
-def initialize_sales_bot(vector_store_dir: str="real_estates_sale"):
+def initialize_sales_bot(vector_store_dir: str="education_middle_high_school"):
     db = FAISS.load_local(vector_store_dir, OpenAIEmbeddings(), allow_dangerous_deserialization=True)
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
     
@@ -20,29 +20,12 @@ def initialize_sales_bot(vector_store_dir: str="real_estates_sale"):
 
     return SALES_BOT
 
-def real_estates_sale_chat(message, history):
+def sales_chat(message, history):
     print(f"[message]{message}")
     print(f"[history]{history}")
     # TODO: 从命令行参数中获取
     enable_chat = True
-    SALES_BOT = initialize_sales_bot("real_estates_sale")
-    ans = SALES_BOT({"query": message})
-    # 如果检索出结果，或者开了大模型聊天模式
-    # 返回 RetrievalQA combine_documents_chain 整合的结果
-    if ans["source_documents"] or enable_chat:
-        print(f"[result]{ans['result']}")
-        print(f"[source_documents]{ans['source_documents']}")
-        return ans["result"]
-    # 否则输出套路话术
-    else:
-        return "我需要和上级和别的同事沟通后回答您这个问题。"
-    
-def middle_high_school_education_chat(message, history):
-    print(f"[message]{message}")
-    print(f"[history]{history}")
-    # TODO: 从命令行参数中获取
-    enable_chat = True
-    SALES_BOT = initialize_sales_bot("education_middle_high_school")
+
     ans = SALES_BOT({"query": message})
     # 如果检索出结果，或者开了大模型聊天模式
     # 返回 RetrievalQA combine_documents_chain 整合的结果
@@ -56,32 +39,18 @@ def middle_high_school_education_chat(message, history):
     
 
 def launch_gradio():
-    real_estate = gr.ChatInterface(
-        fn=real_estates_sale_chat,
-        title="房产销售问答",
-        # retry_btn=None,
-        # undo_btn=None,
-        chatbot=gr.Chatbot(height=600),
-    )
-    middle_high_education = gr.ChatInterface(
-        fn=middle_high_school_education_chat,
+    demo = gr.ChatInterface(
+        fn=sales_chat,
         title="初高中教育问答",
         # retry_btn=None,
         # undo_btn=None,
         chatbot=gr.Chatbot(height=600),
     )
 
-    demo = gr.TabbedInterface(
-          [real_estate, middle_high_education], 
-          ["房产销售", "初中高中教育咨询"]
-        )
-
     demo.launch(share=True, server_name="0.0.0.0")
-
 
 if __name__ == "__main__":
     # 初始化房产销售机器人
-    # initialize_sales_bot()
+    initialize_sales_bot()
     # 启动 Gradio 服务
     launch_gradio()
-
